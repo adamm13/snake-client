c63464afe4bbd875ac7f70b15651bd10ea71d8a4
Adam Mohammed <13.adamm@gmail.com>
2021-02-10 15:49:42 -0500

    added handleUserInput function

--- play.js
+++ play.js
@@ -1,3 +1,27 @@
 const { connect } = require('./client.js');
 console.log('Connecting ...');
+
 connect();
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    //console.log("text")
+    process.exit();
+  }
+   //console.log(key)
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  // handleUserInput is the data callback as we want to exit the game using
+  // CTRL+C. And moves
+  stdin.on('data', key => handleUserInput(key));
+  return stdin;
+
+}
+
+setupInput();
+
