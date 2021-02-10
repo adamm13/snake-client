6cb43be0834fd56a8fa64883686f07165d0b0d6e
Adam Mohammed <13.adamm@gmail.com>
2021-02-10 17:58:39 -0500

    updated input to take controls

--- input.js
+++ input.js
@@ -1,12 +1,31 @@
+// Stores the active TCP connection object.
+let connection;
+
+// Setup User Interface 
+// Specifically, so that we can handle user input via stdin
+
 const handleUserInput = function(key) {
-  if (key === "\u0003") {
-    //console.log("text")
+  if (key === '\u0003') {
+    //console.log('text')
     process.exit();
   }
    //console.log(key)
+  if (key === 'w') {
+    connection.write('Move: up');
+ }
+  if (key === 's') {
+    connection.write('Move: down');
+ }
+  if (key === 'a') {
+    connection.write('Move: left');
+ }
+  if (key === 'd') {
+    connection.write('Move: right');
 }
+};
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -16,8 +35,8 @@ const setupInput = function() {
   stdin.on('data', key => handleUserInput(key));
   return stdin;
 
-}
+};
 
 module.exports = {  setupInput
 
-}
+};
